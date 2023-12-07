091bece8c6761cc34e7b439e3535b000b52f1842
ricardospg <ricardospgc@gmail.com>
2023-12-06 21:41:39 -0300

    JAVA TP04Q02 - 100%

--- Aeds-II/Trabalhos-Praticos/TP4/java/Q02-Arvore de Arvores/Jogador.java
+++ Aeds-II/Trabalhos-Praticos/TP4/java/Q02-Arvore de Arvores/Jogador.java
@@ -12,31 +12,144 @@ import java.io.IOException;
 */
 
 class No {
-    public Jogador jogador;
+    public int alturaChave;
     public No esq, dir;
+    public No2 raiz2;
 
     /* Construtores */
-    public No(Jogador jogador) {
-        this(jogador, null, null);
+
+    public No(int alturaChave) {
+        this(alturaChave, null, null, null);
     }
 
-    public No(Jogador jogador, No esq, No dir) {
-        this.jogador = jogador.cloneJogador();
+    public No(int altura, No esq, No dir, No2 raiz2) {
+        this.alturaChave = altura;
         this.esq = esq;
         this.dir = dir;
+        this.raiz2 = raiz2;
     }
 }// fim class No
 
-class Arvore { // Organizada baseada no nome do Jogador
+class No2 {
+	public Jogador jogador; // Conteudo do no.
+	public No2 esq; // No da esquerda.
+	public No2 dir; // No da direita.
+	
+    /* Construtores */
+    
+	No2(Jogador jogador) {
+		this(jogador, null, null);
+	}
+
+	No2(Jogador jogador, No2 esq, No2 dir) {
+		this.jogador = jogador.cloneJogador();
+        this.esq = esq;
+        this.dir = dir;
+	}
+}
+
+class ArvoreArvore { // Externamente baseada na altura e internamente no nome
     private No raiz;
 
     static Long tempoTotal = (long) 0;
     static Integer nComp = 0;
 
     /* Construtor */
-    Arvore() {
+    ArvoreArvore() {
         raiz = null;
+        inserir(7);
+        inserir(3);
+        inserir(11);
+        inserir(1);
+        inserir(5);
+        inserir(9);
+        inserir(12);
+        inserir(0);
+        inserir(2);
+        inserir(4);
+        inserir(6);
+        inserir(8);
+        inserir(10);
+        inserir(13);
+        inserir(14);
     }
+
+    /*** Inserir ***/
+
+    /**
+     * Metodo publico para inserir a chave das alturas na arvore externa.
+     * @param int alturaChave
+     * @throws Exception
+     */
+     public void inserir(int alturaChave) {
+        raiz = inserir(alturaChave, raiz);
+    }// fim inserir()
+
+    /**
+     * Metodo privado para inserir as alturas recursivamente na arvore externa
+     * @param altura
+     * @param i
+     * @return
+     */
+    private No inserir(int altura, No i) {
+        if (i == null) {
+            i = new No(altura);
+        } else if (altura < i.alturaChave) {
+            i.esq = inserir(altura, i.esq);
+        } else if (altura > i.alturaChave) {
+            i.dir = inserir(altura, i.dir);
+        } else {
+            MyIO.println("Erro ao inserir Nó!");
+        }
+        return i;
+    }// fim inserir()
+
+    /**
+     * Metodo publico para inserir um jogador na arvore
+     * @param j - Jogador a ser inserido
+     */
+    public void inserir(Jogador j) {
+        inserir(j, raiz);
+    }// fim inserir()
+
+    /**
+     * Metodo privado que procura qual No tem a altura correspondente
+     * a do Jogador a ser inserido
+     * @param j - jogador a ser inserido
+     * @param i - no atual
+     */
+    private void inserir(Jogador j, No i) {
+        if (i == null) {
+            MyIO.println("Erro ao inserir");
+        } else if ((j.getAltura() % 15) < (i.alturaChave)) {
+            nComp++;
+            inserir(j, i.esq);
+        } else if ((j.getAltura() % 15) > i.alturaChave) {
+            nComp++;
+            inserir(j, i.dir);
+        } else {
+            i.raiz2 = inserirAA(j, i.raiz2);
+        }
+    }// fim inserir()
+
+    /**
+     * Metodo privado que insere o jogador na arvore interna baseado no nome
+     * @param j - Jogador a ser inserido
+     * @param i - no atual
+     * @return - posicao que o jogador foi inserido
+     */
+    private No2 inserirAA(Jogador j, No2 i) {
+        if (i == null) {
+            i = new No2(j);
+        } else if (j.getNome().compareTo(i.jogador.getNome()) < 0) {
+            i.esq = inserirAA(j, i.esq);
+        } else if (j.getNome().compareTo(i.jogador.getNome()) > 0) {
+            i.dir = inserirAA(j, i.dir);
+        } else {
+            MyIO.println("Erro ao inserir, elemento existente");
+        }
+        return i;
+    }// fimn inserirAA()
 
     /*** Pesquisas ***/
 
@@ -50,8 +163,6 @@ class Arvore { // Organizada baseada no nome do Jogador
 
         while (!isFim) {
             
-            
-            
             long tempoInicio = System.nanoTime();
             
             MyIO.print(chave + " raiz "); // informa o nome do jogador a ser pesquisado
@@ -59,8 +170,7 @@ class Arvore { // Organizada baseada no nome do Jogador
             MyIO.println((resp == true) ? "SIM" : "NAO"); // informa se foi encontrado
 
             long tempoFim = System.nanoTime();
-            tempoTotal += (tempoFim - tempoInicio);
-    
+            tempoTotal += (tempoFim - tempoInicio) / 1000000;
 
             chave = MyIO.readLine();
             if (chave.equals("FIM")) isFim = true;
@@ -74,205 +184,39 @@ class Arvore { // Organizada baseada no nome do Jogador
      * @param No     raiz - raiz da Arvore
      * @return boolean com o resultado da pesquisa
      */
-    public boolean pesquisar(String chave, No raiz) {        
+    private boolean pesquisar(String chave, No i) {        
         boolean resp = false;
 
-        if (raiz == null){
-            resp = false;
-            nComp++;
+        if (i != null) {
+            resp = pesquisarAA(chave, i.raiz2);
+            if(!resp){
+                MyIO.print("esq ");
+                resp = pesquisar(chave, i.esq);
+            }
+            if(!resp){
+                MyIO.print("dir ");
+                resp = pesquisar(chave, i.dir);
+            }
         }
-        else if (chave.equals(raiz.jogador.getNome())){
-            resp = true;
-            nComp++;
-        }
-
-        else if (chave.compareTo(raiz.jogador.getNome()) < 0){
-            MyIO.print("esq ");
-            nComp++;
-            resp = pesquisar(chave, raiz.esq);
-        }
-
-        else{
-            MyIO.print("dir ");
-            nComp++;
-            resp = pesquisar(chave, raiz.dir);
-        }
-
 
         return resp;
     } // fim pesquisar()
 
-    /*** Inserir ***/
-
-    /**
-     * Metodo publico para inserir um valor
-     * 
-     * @param valor
-     * @throws Exception se o jogador já existir
-     */
-    public void inserir(Jogador jog) throws Exception {
-        raiz = inserir(jog, raiz);
-    } // fim inserir()
-
-    /**
-     * Metodo privado recursivo para inserir um jogador
-     * 
-     * @param Jogador jog
-     * @param No  i
-     * @throws Exception se o jogador já existir
-     */
-    private No inserir(Jogador jog, No i) throws Exception {
-
-        if (i == null){ 
-            i = new No(jog);
+    private boolean pesquisarAA(String chave, No2 i){
+        boolean resp = false;
+        if (i != null) {
+            resp = i.jogador.getNome().equals(chave);
+            if (!resp) {
+                MyIO.print("ESQ ");
+                resp = pesquisarAA(chave, i.esq);
+            }
+            if (!resp) {
+                MyIO.print("DIR ");
+                resp = pesquisarAA(chave, i.dir);
+            }       
         }
-        else if (jog.getNome().compareTo(i.jogador.getNome()) < 0) {
-            i.esq = inserir(jog, i.esq);
-        }
-        else if (jog.getNome().compareTo(i.jogador.getNome()) > 0) {
-            i.dir = inserir(jog, i.dir);
-        }
-        else 
-            throw new Exception("jogador ja existe!");
-
-        return i;
-    } // fim inserir()
-
-    /*** Remocoes ***/
-
-    /**
-     * Metodo publico para remover o jogador parametrizado
-     * 
-     * @param valor a ser removido
-     * @throws Exception se o valor nao existe
-     */
-    public void remover(Jogador valor) throws Exception {
-        raiz = remover(valor, raiz);
-    } // fim remover()
-
-    /**
-     * Metodo privado recursivo para remover jogador.
-     * 
-     * @param valor jogador a ser removido.
-     * @param i     No em analise.
-     * @return No em analise, alterado ou nao.
-     * @throws Exception Se nao encontrar jogador.
-     */
-    private No remover(Jogador valor, No i) throws Exception {
-        if (i == null)
-            throw new Exception("jogador nao existe!");
-
-        else if (valor.getNome().compareTo(raiz.jogador.getNome()) > 0)
-            i.dir = remover(valor, i.dir);
-
-        else if (valor.getNome().compareTo(raiz.jogador.getNome()) < 0)
-            i.esq = remover(valor, i.esq);
-
-        // A partir daqui, tratamento de quando o jogador foi encontrado;
-        // É preciso identificar se o jogador a ser removido tem 0 filhos,
-        // 1 filho e seu lado, ou 2 filhos;
-
-        // No a ser removido tem 0 filhos ou 1 filho à esquerda, 
-        // "sobe" com o filho à esquerda;
-        else if (i.dir == null)
-            i = i.esq;
-
-        // No a ser removido tem 1 filho à direita, "sobe" com o filho à direita;
-        else if (i.esq == null)
-            i = i.dir;
-
-        /*
-         * No a ser removido tem 2 filhos.
-         * Nesse caso, o jogador removido deve ser substituido pelo
-         * maior no da subarvore esquerda ou menor no da subarvore direita
-         * a fim de manter a propriedade de AB de pesquisa.
-         * Aqui, substitui pelo maior da esquerda.
-         */
-        else
-            i.esq = maiorEsq(i, i.esq); // encontra o substituto, o maior no da subarvore esquerda.
-
-        return i;
-
-    } // fim remover()
-
-    /**
-     * Metodo para trocar o jogador "removido" pelo maior da esquerda.
-     * 
-     * @param i No que teve o jogador removido.
-     * @param k No da subarvore esquerda.
-     * @return No em analise, alterado ou nao.
-     */
-    private No maiorEsq(No i, No k) {
-
-        // Encontrou o maximo da subarvore esquerda.
-        if (k.dir == null) {
-            i.jogador = k.jogador.cloneJogador(); // Substitui i por k.
-            k = k.esq; // Substitui k por k.ESQ.
-
-            // Existe no a direita.
-        } else {
-            // Caminha para direita.
-            k.dir = maiorEsq(i, k.dir);
-        }
-        return k;
-    }// fim maiorEsq()
-
-    /**
-     * Retorna o maior jogador da árvore
-     * @return resp
-     */
-    public Jogador getMaior() {
-        Jogador resp = new Jogador();
-
-        if (raiz != null) {
-            No i;
-            for (i = raiz; i.dir != null; i = i.dir); // caminha até o jogador mais à direita
-            resp = i.jogador.cloneJogador();
-        }
-
         return resp;
-    } // fim getMaior()
-
-    /**
-     * Retorna o menor jogador da árvore
-     * @return resp
-     */
-    public Jogador getMenor() {
-        Jogador resp = new Jogador();
-
-        if (raiz != null) {
-            No i;
-            for (i = raiz; i.esq != null; i = i.esq); // caminha até o jogador mais à esquerda
-            resp = i.jogador.cloneJogador();
-        }
-
-        return resp;
-    } // fim getMaior()
-
-    /**
-     * Metodo que retorna a altura da árvore
-     * 
-     * @return int altura da árvore
-     */
-    public int getAltura() {
-        return getAltura(raiz, 0);
-    } // fim getAltura()
-
-    /**
-     * Metodo que retorna a altura da árvore
-     * 
-     * @return int altura da árvore
-     */
-    public int getAltura(No i, int altura) {
-        if (i == null) {
-            altura--;
-        } else {
-            int alturaEsq = getAltura(i.esq, altura + 1);
-            int alturaDir = getAltura(i.dir, altura + 1);
-            altura = (alturaEsq > alturaDir) ? alturaEsq : alturaDir;
-        }
-        return altura;
-    } // fim getAltura()
+    }//fim pesquisar()
 
     /**
      * armazena os jogadores na Arvore
@@ -299,26 +243,13 @@ class Arvore { // Organizada baseada no nome do Jogador
         }
     } // fim adicionaJogadores()
 
-    public void caminharCentral() {
-		caminharCentral(raiz);
-
-	} // fim caminharCentral()
-
-	private void caminharCentral(No i) {
-		if (i != null) {
-            caminharCentral(i.esq);
-			MyIO.println(i.jogador.getNome());
-			caminharCentral(i.dir);
-		}
-	} // fim caminharCentral()
-
     /**
      * Cria um log com o tempo de execucao da pesquisa
      * e quantidade de movimentacoes
      */
     public void criaLog(){
 
-        String fileName = "803833_arvoreBinaria";
+        String fileName = "803833_arvoreArvore.txt";
         String matricula = "803833";
 
         try{
@@ -330,7 +261,7 @@ class Arvore { // Organizada baseada no nome do Jogador
                 FileWriter fw = new FileWriter(fileName, false); // false = rewrite
                 BufferedWriter bw = new BufferedWriter(fw);
 
-                bw.write(matricula + '\t' + nComp + '\t' + tempoTotal + "us");
+                bw.write(matricula + '\t' + nComp + '\t' + tempoTotal+"ms");
 
                 bw.close();
             } catch (IOException e){
@@ -523,12 +454,12 @@ public class Jogador {
     /***************************************************************************************/
 
     public static void main(String[] args) throws Exception {
-        Arvore arvoreAB = new Arvore();
+        ArvoreArvore arvore = new ArvoreArvore();
 
-        arvoreAB.adicionaJogadores();
-        arvoreAB.pesquisar();
+        arvore.adicionaJogadores();
+        arvore.pesquisar();
 
-        arvoreAB.criaLog();
+        arvore.criaLog();
     }// fim main
 
 }// fim classe Jogador
