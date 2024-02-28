3573ac1258633a9afdb484d083f75878f2e88e98
ricardospg <ricardospgc@gmail.com>
2024-02-28 15:41:32 -0300

    TP01 - Q04 Alteração Aleatória java

---
+++ 3o Período/Aeds-II/TP01/Q04-AlteraçãoAleatóriaJava/Alteracao.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Alteracao{
+
+   /**
+    * Metodo para avaliar se o input = "FIM"
+    * @param input string contendo input do usuario
+    * @return booleano se e' fim ou nao
+    */
+    static public boolean ehFim(String input){
+        boolean resp = false;
+        if(input.charAt(0) == 'F' && input.charAt(1) == 'I' && input.charAt(2) == 'M')
+            resp = true;
+        return resp;
+    }// fim ehFim
+    
+    static public String alteracao(String input, Random gerador){
+        
+        char char1 = (char)('a' + (Math.abs(gerador.nextInt()) % 26));
+        char char2 = (char)('a' + (Math.abs(gerador.nextInt()) % 26));
+
+        StringBuilder resp = new StringBuilder();
+
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == char1) 
+                resp.append(char2);
+            else 
+                resp.append(input.charAt(i));
+        }
+
+        return resp.toString();
+    }// fim alteracao()
+
+    public static void main(String[] args)  {
+        Random gerador = new Random();
+        gerador.setSeed(4);
+
+        String input = MyIO.readLine();
+
+        while (!ehFim(input)) {
+             MyIO.println(alteracao(input, gerador));
+
+             input = MyIO.readLine();
+        } 
+
+    }// fim main()  
+}
