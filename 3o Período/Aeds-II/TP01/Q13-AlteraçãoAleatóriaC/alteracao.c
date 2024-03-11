4c5d0e0c80244cf49ee99cb0f2bbfc8a81773da7
ricardospg <ricardospgc@gmail.com>
2024-03-11 15:51:36 -0300

    TP Q13 - alteração em C, 80%

--- 3o Período/Aeds-II/TP01/Q13-AlteraçãoAleatóriaC/alteracao.c
+++ 3o Período/Aeds-II/TP01/Q13-AlteraçãoAleatóriaC/alteracao.c
@@ -3,11 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAXTAM 100
+#define MAXTAM 5000
 
 bool ehFim(char []);
 char* alteracao(char []);
-char* alteracaoRec(char [], char, char ,int );
+char* alteracaoRec(char [] ,int );
 
 /**
  *  Funcao que verifica se a entrada 'e igual a "FIM"
@@ -23,22 +23,23 @@ bool ehFim(char input[]){
  * Chama a funcao recursiva que analisa se e' palindromo
 */
 char* alteracao(char input[MAXTAM]) {
-    char char1 = (char)('a' + (rand() % 26));
-    char char2 = (char)('a' + (rand() % 26));
-    return alteracaoRec(input, char1, char2, 0);
+    return alteracaoRec(input, 0);
 }// fim alteracao
 
 /**
  * Retorna se a string de entrada e' um palindromo
 */
-char* alteracaoRec(char input[MAXTAM], char char1, char char2, int index){
+char* alteracaoRec(char input[MAXTAM], int index){
+    char original = ('a' + rand() % 26);
+    char nova = ('a' + rand() % 26);
+
     if(index < strlen(input)){
-        if(input[index] == char1) input[index] = char2;
-        
-        alteracaoRec(input, char1, char2, index + 1);
+        if(input[index] == original){
+            input[index] = nova;
+        }
+        alteracaoRec(input, index+1);
     }
     return input;
-
 }//fim alteracaoRec()
 
 int main(){
@@ -48,14 +49,11 @@ int main(){
     fgets(input, sizeof(input), stdin);
 
     while(!ehFim(input)){
-
         input[strlen(input) - 1] = '\0'; // retira o '\n' do fim
 
         printf("%s\n", alteracao(input)); 
 
         fgets(input, sizeof(input), stdin);
     }
-
-
     return 0;
 }//fim main
