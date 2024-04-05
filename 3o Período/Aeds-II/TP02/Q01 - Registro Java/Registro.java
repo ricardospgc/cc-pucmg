8edfc4b521674d44639a979b96791f64d5aa11a7
ricardospg <ricardospgc@gmail.com>
2024-04-04 22:21:34 -0300

    TP02 - Q01 FINALIZADA (pendente alterações de formatação VERDE)

--- 3o Período/Aeds-II/TP02/Q01 - Registro Java/Registro.java
+++ 3o Período/Aeds-II/TP02/Q01 - Registro Java/Registro.java
@@ -69,6 +69,9 @@ class Character{
     public void setAlternate_names(ArrayList<String> alternate_names){ 
         this.alternate_names = new ArrayList<String>(alternate_names);}
     public ArrayList<String> getAlternate_names(){ return this.alternate_names;}
+    public void setAlternate_names(String alternate_names){
+        setAlternate_names(StringToArrayList(alternate_names));
+    }// setAlternate_names(String)
 
     // house
     public void setHouse(String house){ this.house = house;}
@@ -89,10 +92,16 @@ class Character{
     // hogwarts staff
     public void setHogwartsStaff(boolean hogwartsStaff){ this.hogwartsStaff = hogwartsStaff;}
     public Boolean getHogwartsStaff(){ return this.hogwartsStaff;}
+    public void setHogwartsStaff(String hogwartsStaff){
+        setHogwartsStaff(StringToBoolean(hogwartsStaff));
+    }// setHogwartsStaff(String)
 
     // hogwarts student
     public void setHogwartsStudent(boolean hogwartsStudent){ this.hogwartsStudent = hogwartsStudent;}
     public Boolean getHogwartsStudent(){ return this.hogwartsStudent;}
+    public void setHogwartsStudent(String hogwartsStudent){
+        setHogwartsStudent(StringToBoolean(hogwartsStudent));
+    }// setHogwartsStudent(String)
 
     // actor name
     public void setActorName(String actorName){ this.actorName = actorName;}
@@ -101,23 +110,35 @@ class Character{
     // alive
     public void setAlive(boolean alive){ this.alive = alive;}
     public Boolean getAlive(){ return this.alive;}
+    public void setAlive(String alive){
+        setAlive(StringToBoolean(alive));
+    }// setAlive(String)
 
     // alternate actor
     public void setAlternate_actors(ArrayList<String> alternate_actors){ 
         this.alternate_actors = new ArrayList<String>(alternate_actors);}
     public ArrayList<String> getAlternate_actors(){ return this.alternate_actors;}
+    public void setAlternate_actors(String alternate_actors){
+        setAlternate_actors(StringToArrayList(alternate_actors));
+    }// setAlternate_actors(String)
 
     // date of birth
     public void setDateOfBirth(LocalDate dateOfBirth){ this.dateOfBirth = dateOfBirth;}
     public LocalDate getDateOfBirth(){ return this.dateOfBirth;}
+    public void setDateOfBirth(String dateOfBirth){
+        setDateOfBirth(LocalDate.parse(formatDate(dateOfBirth)));
+    }// setDateOfBirth(String)
 
     // year of birth
     public void setYearOfBirth(int yearOfBirth){ this.yearOfBirth = yearOfBirth;}
     public Integer getYearOfBirth(){ return this.yearOfBirth;}
+    public void setYearOfBirth(String yearOfBirth){
+        setYearOfBirth(Integer.parseInt(yearOfBirth));
+    }// setYearOfBirth(String)
 
     // eye color
     public void setEyeColor(String eyeColor){ this.eyeColor = eyeColor;}
-    public String getEyeOfColor(){ return this.eyeColor;}
+    public String getEyeColor(){ return this.eyeColor;}
 
     // gender
     public void setGender(String gender){ this.gender = gender;}
@@ -130,6 +151,58 @@ class Character{
     // wizard
     public void setWizard(boolean wizard){ this.wizard = wizard;}
     public Boolean getWizard(){ return this.wizard; }
+    public void setWizard(String wizard){setWizard(StringToBoolean(wizard));}
+    
+    /**
+     * Converte string para booleano (parseBoolean)
+     * @param str
+     * @return true se for "True", else false
+     */
+    static public Boolean StringToBoolean(String str){
+        return (str.equals("True")) ? true : false;
+    }// StringToBoolean()
+
+    /**
+     * Converte string para ArrayList
+     * @param str
+     * @return ArrayList com as strings separadas
+     */
+    static public ArrayList<String> StringToArrayList(String str){        
+        str = str.replace("[","");
+        str = str.replace("]", "");
+
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(str.split(",")));
+
+        return list;
+    }// StringToArrayList()
+
+    /**
+     * Converte DMY - YMD e vice-versa
+     * @param str
+     * @return string de data invertida
+     */
+    static public String formatDate(String str){
+        String[] split = str.split("-");
+        str = split[2] + "-" + split[1] + "-" + split[0];
+        return str;
+    }// formatDate()
+
+    /**
+     * Converte o arraylist em string no formato do Verde
+     * @param arr
+     * @return
+     */
+    public String ArrayListToString(ArrayList<String> arr){
+        String str = "[";
+        
+        for(int i = 0; i < arr.size(); i++){
+            str += arr.get(i) + ",";
+        }
+
+        str = str.substring(0, str.length()-1) + "]";
+
+        return str;
+    }// ArrayListToString()
 
     /**
      * separa a string info em um array
@@ -137,9 +210,34 @@ class Character{
      * @return array com as informacoes separadas
      */
     private String[] splitInfo(String strInfo){
-        String[] arrInfos = new String[18];
-
+        String[] arrInfos = new String[18]; // 18 espaços para os 18 atributos
+        String[] alternateInfos = new String[2]; // alternate names e alternate actors
         
+
+        strInfo = strInfo.replace("\"", ""); // retirando as aspas
+        StringBuilder sbAux = new StringBuilder(strInfo);
+        
+        // Trata alternate names e alternate actors
+        int c = 2;
+        do{
+            int idxOfOpenBracket = strInfo.indexOf("[");
+            int idxOfCloseBracket = strInfo.indexOf("]");
+    
+            if(idxOfOpenBracket+1 != idxOfCloseBracket) // Se true, tem conteudo
+                alternateInfos[alternateInfos.length - c] = strInfo.substring(idxOfOpenBracket, idxOfCloseBracket+1);
+            else // nao tem / nao informado
+                alternateInfos[alternateInfos.length - c] = "nao informado";
+            // remove a string, +2 para incluir a virgula
+            strInfo = sbAux.delete(idxOfOpenBracket, idxOfCloseBracket+2).toString();
+
+            c--;
+        } while(c > 0);
+
+        //Preenche arrInfos com os atributos
+        strInfo += ", , ";
+        arrInfos = strInfo.split(",");
+        arrInfos[16] = alternateInfos[0];
+        arrInfos[17] = alternateInfos[1];
 
         return arrInfos;
     }
@@ -149,24 +247,64 @@ class Character{
      * @param info
      */
     private void setCharacter(String strInfo){
-
-        MyIO.println(strInfo);
         
         String[] arrInfos = new String[18];
         arrInfos = splitInfo(strInfo);
 
-        /* 
-        setId(split[0]);
-        setName(split[1]);
-        //setAlternate_names(split[2]);
-        setHouse(split[3]);
-        setAncestry(split[4]);
-        setSpecies(split[5]);
-        setPatronus(split[6]);
-        set*/
-         
+        setId(arrInfos[0]);
+        setName(arrInfos[1]);
+        setHouse(arrInfos[2]);
+        setAncestry(arrInfos[3]);
+        setSpecies(arrInfos[4]);
+        setPatronus(arrInfos[5]);
+        setHogwartsStaff(arrInfos[6]);
+        setHogwartsStudent(arrInfos[7]);
+        setActorName(arrInfos[8]);
+        setAlive(arrInfos[9]);
+        setDateOfBirth(arrInfos[10]);
+        setYearOfBirth(arrInfos[11]);
+        setEyeColor(arrInfos[12]);
+        setGender(arrInfos[13]);
+        setHairColor(arrInfos[14]);
+        setWizard(arrInfos[15]);
+
+        setAlternate_names(arrInfos[16]);
+        setAlternate_actors(arrInfos[17]);
 
     }// setCharacter()
+
+    /**
+     * printa um personagem da lista, baseado no index
+     * @param index do personagem printado
+     */
+    public void printCharacter(){
+        MyIO.print(getId() + " ## ");
+        MyIO.print(getName() + " ## ");
+
+        MyIO.print(ArrayListToString(getAlternate_names()) + " ## ");
+
+        MyIO.print(getHouse() + " ## ");
+        MyIO.print(getAncestry() + " ## ");
+        MyIO.print(getSpecies() + " ## ");
+        MyIO.print(getPatronus() + " ## ");
+        MyIO.print(getHogwartsStaff() + " ## ");
+        MyIO.print(getHogwartsStudent() + " ## ");
+        MyIO.print(getActorName() + " ## ");
+        MyIO.print(getAlive() + " ## ");
+
+        MyIO.print(ArrayListToString(getAlternate_actors()) + " ## ");
+
+        MyIO.print(formatDate(getDateOfBirth().toString()) + " ## ");
+
+        MyIO.print(getYearOfBirth() + " ## ");
+        MyIO.print(getEyeColor() + " ## ");
+        MyIO.print(getGender() + " ## ");
+        MyIO.print(getHairColor() + " ## ");
+        MyIO.println(getWizard());
+
+        
+        
+    }// printCharacter()
 
 }// class Character
 
@@ -248,23 +386,15 @@ public class Registro {
     public void addCharacter(String id){
         String info = getCharacterInfo(id);
         Character character = new Character(info);
-        //characterList.add(character);
+        characterList.add(character);
     }// addCharacter()
-
-    /**
-     * printa um personagem da lista, baseado no index
-     * @param index do personagem printado
-     */
-    public void printCharacter(int index){
-        MyIO.println(characterList.get(0).toString());
-    }// printCharacter()
 
     /**
      * Printa todos os personagens da lista
      */
     public void printCharacterList(){
         for(int i = 0; i < characterList.size(); i++){
-            MyIO.println(characterList.get(i).toString());
+            characterList.get(i).printCharacter();
         }
     }// printCharacterList()
 
@@ -274,10 +404,11 @@ public class Registro {
         String id = MyIO.readLine();
         while(!isFim(id)){
             registro.addCharacter(id);
-            //registro.printCharacterList();
 
             id = MyIO.readLine();
         }
+
+        registro.printCharacterList();
 
     }// main()
 }
