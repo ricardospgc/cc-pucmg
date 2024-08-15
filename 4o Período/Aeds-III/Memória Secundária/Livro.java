c675e9fc8a66667c16675fd0c2915bd32c318ab3
ricardospg <ricardospgc@gmail.com>
2024-08-15 16:49:49 -0300

    Continuação do estudo de Memória Secundária (3/5)

--- 4o Período/Aeds-III/Memória Secundária/Livro.java
+++ 4o Período/Aeds-III/Memória Secundária/Livro.java
@@ -1,3 +1,8 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class Livro {
@@ -5,6 +10,13 @@ public class Livro {
     protected String titulo;
     protected String autor;
     protected float preco;
+
+    public Livro(){
+        this.idLivro = -1;
+        this.titulo = "";
+        this.autor = "";
+        this.preco = 0F;
+    }
 
     public Livro(int id, String titulo, String autor, float preco){
         this.idLivro = id;
@@ -20,5 +32,37 @@ public class Livro {
                "\n Titulo: " + titulo +
                "\n Autor: " + autor +
                "\n Preco: R$" + df.format(preco);
+    }
+
+    /**
+     * converte os atributos do objeto corrente em bytes
+     * @return byte[] com os dados convertidos
+     * @throws IOException
+     */
+    public byte[] toByteArray() throws IOException{
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+
+        dos.writeInt(idLivro);
+        dos.writeUTF(titulo);
+        dos.writeUTF(autor);
+        dos.writeFloat(preco);
+
+        return baos.toByteArray();
+    }
+
+    /**
+     * armazena os campos do byte[] nos atributos equivalentes do objeto corrente
+     * @param ba byte[] a ser interpretado
+     * @throws IOException
+     */
+    public void fromByteArray(byte[] ba) throws IOException{
+        ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+        DataInputStream dis = new DataInputStream(bais);
+
+        idLivro = dis.readInt();
+        titulo = dis.readUTF();
+        autor = dis.readUTF();
+        preco = dis.readFloat();
     }
 }
--- 4o Período/Aeds-III/Memória Secundária/Main.java
+++ 4o Período/Aeds-III/Memória Secundária/Main.java
@@ -1,32 +1,67 @@
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Livro l1 = new Livro(1, "Eu Robô", "Isaac Asimov", 14.9F);
         Livro l2 = new Livro(2, "Eu Sou A Lenda", "Richard Matheson", 21.99F);
 
-        System.out.println(l1.toString());
-        System.out.println(l2.toString());
+        System.out.println(l1);
+        System.out.println(l2);
 
-        FileOutputStream arq;
+        FileOutputStream arqOut;
         DataOutputStream dos;
 
-        try{
-            arq = new FileOutputStream("dados/livros.db");
-            dos = new DataOutputStream(arq);
+        FileInputStream arqIn;
+        DataInputStream dis;
 
-            dos.writeInt(l1.idLivro);
-            dos.writeUTF(l1.titulo);
-            dos.writeUTF(l1.autor);
-            dos.writeFloat(l1.preco);
+        byte[] ba;
 
-            dos.writeInt(l2.idLivro);
-            dos.writeUTF(l2.titulo);
-            dos.writeUTF(l2.autor);
-            dos.writeFloat(l2.preco);
+        try {
+            /*** Escrita ***/
+            arqOut = new FileOutputStream("dados/livros.db");
+            dos = new DataOutputStream(arqOut);
 
-            arq.close();
+            ba = l1.toByteArray(); // armazena em ba a informacao em bytes
+            dos.writeInt(ba.length); // escreve o indicador de tamanho de registro
+            dos.write(ba); // escreve o registro em si
+
+            ba = l2.toByteArray(); // armazena em ba a informacao em bytes
+            dos.writeInt(ba.length); // escreve indicador de tamanho de registro
+            dos.write(ba); // escreve o registro em si
+
+            dos.close();
+            arqOut.close();
+
+            /*** Leitura ***/
+
+            Livro l3 = new Livro();
+            Livro l4 = new Livro();
+            int tam; // indicador de tamanho do registro
+            arqIn = new FileInputStream("dados/livros.db");
+            dis = new DataInputStream(arqIn);
+
+            // atualiza ba para o registro a ser lido
+            tam = dis.readInt(); // le o primeiro int do registro, que e' o indicador de tamanho
+            ba = new byte[tam];
+            dis.read(ba);
+            l3.fromByteArray(ba); // armazena em l3 o registro de ba
+
+
+            // atualiza ba para o registro a ser lido
+            tam = dis.readInt(); // le o primeiro int do registro, que e' o indicador de tamanho
+            ba = new byte[tam];
+            dis.read(ba);
+            l4.fromByteArray(ba); // armazena em l4 o registro de ba
+
+            System.out.println(l3);
+            System.out.println(l4);
+
+            dis.close();
+            arqIn.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
