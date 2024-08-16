39fc4fd4b1e1e44d88917c105f599e5f983da71c
ricardospg <ricardospgc@gmail.com>
2024-08-16 09:37:00 -0300

    Estudo de memória secundária 5/5

--- 4o Período/Aeds-III/Memória Secundária/Main.java
+++ 4o Período/Aeds-III/Memória Secundária/Main.java
@@ -1,7 +1,9 @@
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
+// import java.io.DataOutputStream;
+// import java.io.FileOutputStream;
+// import java.io.DataInputStream;
+// import java.io.FileInputStream;
+
+import java.io.RandomAccessFile;
 
 public class Main {
      public static void main(String[] args) {
@@ -10,6 +12,57 @@ public class Main {
 
         System.out.println(l1);
         System.out.println(l2);
+
+
+        RandomAccessFile arq;
+        byte[] ba;
+
+        try {
+            /* LEITURA E ESCRITA DE ACESSO ALEATORIO ********/
+            
+            arq = new RandomAccessFile("dados/livros.db", "rw"); // o RAF permite leitura, escrita e movimentacoes
+            
+            /* ESCRITA */
+
+            long pos1 = arq.getFilePointer(); // ponteiro do primeiro registro
+            ba = l1.toByteArray(); // armazena em ba a informacao em bytes
+            arq.writeInt(ba.length); // escreve o indicador de tamanho de registro
+            arq.write(ba); // escreve o registro em si
+
+            long pos2 = arq.getFilePointer(); // ponteiro do segundo registro
+            ba = l2.toByteArray(); // armazena em ba a informacao em bytes
+            arq.writeInt(ba.length); // escreve o indicador de tamanho de registro
+            arq.write(ba); // escreve o registro em si
+
+            // como o RAF ja permite tudo que precisamos, nao e' necessario fechar e reabrir o arquivo para leitura
+
+            Livro l3 = new Livro();
+            Livro l4 = new Livro();
+            int tam; // indicador de tamanho do registro
+            
+            // o ponteiro esta no fim do arquivo, onde a escrita terminou
+
+            // atualiza ba para o registro a ser lido
+            arq.seek(pos2); // envia o ponteiro para a posicao do registro
+            tam = arq.readInt(); // le o primeiro int do registro, que e' o indicador de tamanho
+            ba = new byte[tam];
+            arq.read(ba);
+            l3.fromByteArray(ba); // armazena em l3 o registro de ba
+
+            // atualiza ba para o registro a ser lido
+            arq.seek(pos1); // envia o ponteiro para a posicao do registro
+            tam = arq.readInt(); // le o primeiro int do registro, que e' o indicador de tamanho
+            ba = new byte[tam];
+            arq.read(ba);
+            l4.fromByteArray(ba); // armazena em l3 o registro de ba
+
+            System.out.println("---------");
+            System.out.println(l3);
+            System.out.println(l4);
+
+            arq.close();
+
+        /* LEITURA E ESCRITA SEQUENCIAIS *************
 
         FileOutputStream arqOut;
         DataOutputStream dos;
@@ -20,7 +73,7 @@ public class Main {
         byte[] ba;
 
         try {
-            /*** Escrita ***/
+            // Escrita 
             arqOut = new FileOutputStream("dados/livros.db");
             dos = new DataOutputStream(arqOut);
 
@@ -35,8 +88,7 @@ public class Main {
             dos.close();
             arqOut.close();
 
-            /*** Leitura ***/
-
+            // Leitura
             Livro l3 = new Livro();
             Livro l4 = new Livro();
             int tam; // indicador de tamanho do registro
@@ -61,11 +113,11 @@ public class Main {
 
             dis.close();
             arqIn.close();
-
+        */
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        
         
     }
 }
